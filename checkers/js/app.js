315ed9a62543133a406c25de1e5109bc89909501
Benjamin Carpenter <bcarpenter@ucvts.org>
2020-02-20 13:12:33 -0500

    sbhhgdfhjhbj

--- checkers/js/app.js
+++ checkers/js/app.js
@@ -1,28 +1,47 @@
 window.onload= function() {
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("3").textContent = "\u2022"
-    document.getElementById("5").textContent = "\u2022"
-    document.getElementById("7").textContent = "\u2022"
-    document.getElementById("10").textContent = "\u2022"
-    document.getElementById("12").textContent = "\u2022"
-    document.getElementById("14").textContent = "\u2022"
-    document.getElementById("16").textContent = "\u2022"
-    document.getElementById("17").textContent = "\u2022"
-    document.getElementById("19").textContent = "\u2022"
-    document.getElementById("21").textContent = "\u2022"
-    document.getElementById("23").textContent = "\u2022"
+  board = [
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  "", "", "", "", "", "", "", "",
+  ];
+  let blackCheckers = [];
+  let redCheckers = [];
+    document.getElementById("1") = blackCheckers[blackCheckers.length]
+    document.getElementById("3") = blackCheckers[blackCheckers.length]
+    document.getElementById("5") = blackCheckers[blackCheckers.length]
+    document.getElementById("7") = blackCheckers[blackCheckers.length]
+    document.getElementById("10") = blackCheckers[blackCheckers.length]
+    document.getElementById("12") = blackCheckers[blackCheckers.length]
+    document.getElementById("14") = blackCheckers[blackCheckers.length]
+    document.getElementById("16") = blackCheckers[blackCheckers.length]
+    document.getElementById("17") = blackCheckers[blackCheckers.length]
+    document.getElementById("19") = blackCheckers[blackCheckers.length]
+    document.getElementById("21") = blackCheckers[blackCheckers.length]
+    document.getElementById("23") = blackCheckers[blackCheckers.length]
 
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-    document.getElementById("1").textContent = "\u2022"
-
+    document.getElementById("64") = redCheckers[redCheckers.length]
+    document.getElementById("62") = redCheckers[redCheckers.length]
+    document.getElementById("60") = redCheckers[redCheckers.length]
+    document.getElementById("58") = redCheckers[redCheckers.length]
+    document.getElementById("55") = redCheckers[redCheckers.length]
+    document.getElementById("53") = redCheckers[redCheckers.length]
+    document.getElementById("51") = redCheckers[redCheckers.length]
+    document.getElementById("49") = redCheckers[redCheckers.length]
+    document.getElementById("48") = redCheckers[redCheckers.length]
+    document.getElementById("46") = redCheckers[redCheckers.length]
+    document.getElementById("44") = redCheckers[redCheckers.length]
+    document.getElementById("42") = redCheckers[redCheckers.length]
+    win = null;
+    render();
 }
+
+  function render() {
+    board.forEach(function(mark, index) {
+      squares[index].textContent = mark;    // writes an X or an O on board
+    });
+  }
