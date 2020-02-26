e3f1240e21b72dd8ac57ef898524aeb7f7f267f5
Benjamin Carpenter <bcarpenter@ucvts.org>
2020-02-26 13:11:37 -0500

    sbhhgdfhjhbj

--- checkers/js/app.js
+++ checkers/js/app.js
@@ -77,7 +77,7 @@ window.onload= function() {
       let index = squares.findIndex(function(square) {
         return square === e.target;
       });
-      if (board[index] == "\u2022" || board[index] == "♔") {
+      if ((board[index] == "\u2022" || board[index] == "♔")) {
 
       }
     }
